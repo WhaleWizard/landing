df7e4c7fa318f04c0057dd788910ab7f5c27dc4b
Whale Wizard <shoshinruslan97@gmail.com>
2026-04-29 03:18:57 +0500

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,52 +3,8 @@ import type { Env } from './_lib/types';
 const CANONICAL_HOST = 'www.whalewzrd.com';
 const LEGACY_HOSTS = new Set(['whalewzrd.com']);
 
-/*
- * Полная Content Security Policy для всех используемых и потенциально возможных сервисов.
- * Разрешены:
- * - Google Analytics 4, GTM, Google Ads (ремаркетинг и конверсии)
- * - Meta Pixel (браузерный и серверный)
- * - Яндекс Метрика, Яндекс Директ
- * - TikTok Pixel
- * - Twitter/X Pixel
- * - YouTube, Vimeo
- * - Google Fonts
- * - JSONBin, imgbb (i.ibb.co) и собственные ресурсы
- */
-const CSP = [
-  "default-src 'self'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-  "  https://www.googletagmanager.com",
-  "  https://connect.facebook.net",
-  "  https://analytics.tiktok.com",
-  "  https://mc.yandex.ru",
-  "  https://yastatic.net",
-  "  https://static.ads-twitter.com",
-  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-  "img-src 'self' data: https://i.ibb.co https://www.whalewzrd.com",
-  "  https://*.facebook.com https://*.fbcdn.net https://*.twitter.com",
-  "font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com",
-  "connect-src 'self'",
-  "  https://www.google-analytics.com",
-  "  https://*.google-analytics.com",
-  "  https://www.googletagmanager.com",
-  "  https://stats.g.doubleclick.net",
-  "  https://www.googleadservices.com",
-  "  https://*.facebook.com",
-  "  https://*.fbcdn.net",
-  "  https://analytics.tiktok.com",
-  "  https://mc.yandex.ru",
-  "  https://yandex.ru",
-  "  https://api.jsonbin.io",
-  "  https://analytics.twitter.com",
-  "frame-src 'self' https://www.youtube.com https://player.vimeo.com",
-  "media-src 'self' https://*.youtube.com https://*.vimeo.com",
-  "object-src 'none'",
-  "base-uri 'self'",
-].join('; ');
-
+// Только безопасные заголовки, которые НЕ трогают скрипты и пиксели
 const SECURITY_HEADERS: Record<string, string> = {
-  'Content-Security-Policy': CSP,
   'X-Content-Type-Options': 'nosniff',
   'Referrer-Policy': 'strict-origin-when-cross-origin',
   'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
